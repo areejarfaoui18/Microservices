856ffaec2cbfc399591cbb43b0d6c18babe19498
Arij Arfaoui <arij.arfaoui.bp@nttdata.com>
2025-07-20 17:18:24 +0100

    Initial project setup including Maven configuration and base application files

---
+++ src/test/java/com/example/subscriptionpms/SubscriptionPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionPmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
